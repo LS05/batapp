99c89dfb36d9cb89f4d22db724be6f24f0441b73
Rob Wormald <robwormald@gmail.com>
2014-01-15 03:24:55 -0600

    cleaned up, added bower.json and bower_component folder.

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,21 @@
-angular.module('todo', ['ionic']);
+var batApp = angular.module('batapp', ['ionic'])
+
+batApp.controller('mainCtrl', function($scope) {
+  
+	//toggle nav menu thingy
+	//
+	$scope.openLeft = function() {
+          $scope.sideMenuController.toggleLeft();
+     };
+
+
+
+
+
+  $scope.tasks = [
+    { title: 'Collect coins' },
+    { title: 'Eat mushrooms' },
+    { title: 'Get high enough to grab the flag' },
+    { title: 'Find the Princess' }
+  ];
+});
